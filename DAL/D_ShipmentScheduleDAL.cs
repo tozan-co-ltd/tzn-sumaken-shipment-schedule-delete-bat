2426d0b06a43ed3093763e8182160517a3d45237
MyMV <hoangmy1992@gmail.com>
2026-06-19 16:44:25 +0900

    fix: LotNumber取得

--- DAL/D_ShipmentScheduleDAL.cs
+++ DAL/D_ShipmentScheduleDAL.cs
@@ -150,7 +150,7 @@ namespace tzn_sumaken_shipment_schedule_delete_bat.DAL
                 @"
                 SELECT TOP 1 LotNumber
                 FROM D_PrepareShipmentResult
-                WHERE ShipmentScheduleID = @ShipmentScheduleID
+                WHERE ShipmentScheduleID = @ShipmentScheduleID order by PrepareShipmentResultID desc
                 ",
                 new
                 {
